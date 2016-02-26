dd8b24ece4c62acd89a019e947323c44ec21eb57
tpratt12403 <tpratt2@uccs.edu>
2016-02-26 16:38:08 -0700

    HW3 skeleton code

--- project/app/controllers/projects_controller.rb
+++ project/app/controllers/projects_controller.rb
@@ -7,7 +7,26 @@ class ProjectsController < ApplicationController
   end
 
   def index
-    @projects = Project.all
+   sort = params[:sort] || session[:sort]
+    case sort
+    when 'title'
+      ordering, @title_header = {:title => :asc}, 'hilite'
+    when 'date'
+      ordering, @date_header = {:due_date => :asc}, 'hilite'
+    end
+    @all_users = Project.getUsers
+    @selected_users = params[:users] || session[:users] || {}
+    
+    if @selected_users == {}
+      @selected_users = Hash[@all_users.map {|user| [user, user]}]
+    end
+    
+    if params[:sort] != session[:sort] or params[:user] != session[:user]
+      session[:sort] = sort
+      session[:ratings] = @selected_users
+      redirect_to :sort => sort, :users => @selected_users and return
+    end
+    @projects = Project.where(user: @selected_users.keys).order(ordering)
   end
 
   def new
@@ -15,7 +34,7 @@ class ProjectsController < ApplicationController
   end
 
   def create
-    @project = Project.create!(project_params)
+    @project = Project.create!(task_params)
     flash[:notice] = "#{@project.title} was successfully created."
     redirect_to projects_path
   end
@@ -26,7 +45,7 @@ class ProjectsController < ApplicationController
 
   def update
     @project = Project.find params[:id]
-    @project.update_attributes!(project_params)
+    @project.update_attributes!(task_params)
     flash[:notice] = "#{@project.title} was successfully updated."
     redirect_to project_path(@project)
   end
@@ -37,10 +56,9 @@ class ProjectsController < ApplicationController
     flash[:notice] = "Project '#{@project.title}' deleted."
     redirect_to projects_path
   end
-
-  private
-      def project_params
-        params.require(:project).permit(:title, :description, :extended_description, :user, :due_date)
-      end
+  
+  def task_params
+    params.require(:project).permit(:user, :title, :description, :extended_description, :due_date)
+  end
 
 end
